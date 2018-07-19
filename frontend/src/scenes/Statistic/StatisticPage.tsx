950710f14b19b685759254321ddf5d007a4b5c0d
MINSK\Uladzimir_Pantsiukhou <uladzimir.pantsiukhou@gmail.com>
2018-07-19 18:24:09 +0300

    align statistic page according to new Tslint

--- frontend/src/scenes/Statistic/StatisticPage.tsx
+++ frontend/src/scenes/Statistic/StatisticPage.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-
 import { AuthStatus, LoadStatus, FrontEndSnackbarData } from 'models';
 import { AppState, LogoutUser } from 'store';
 
 import { StatisticProps } from './Statistic.model';
 import './Statistic.scss';
 
-import { InitBestUsers, InitMostPopularGames, InitRecentGames } from 'store/statistic';
+import {
+  InitBestUsers,
+  InitMostPopularGames,
+  InitRecentGames
+} from 'store/statistic';
 import { isEmpty } from 'utils';
 
 import { CaSpinner } from 'components/Spinner';
@@ -17,45 +20,52 @@ import { OpenSnackbar, CloseSnackbar } from 'store/snackbar';
 import { CaUsersTables } from 'components/CaUsersTables';
 
 class CaStatisticPageComponent extends React.Component<StatisticProps> {
-
   public dataForSnack: FrontEndSnackbarData[] = [];
   public componentWillReceiveProps(nextProps: StatisticProps): void {
+    const isBestUsersInitFailed =
+      nextProps.statistic.bestUsersStatus === LoadStatus.FAILED &&
+      nextProps.statistic.bestUsersStatus !==
+        this.props.statistic.bestUsersStatus;
+    const isRecentGamesInitFailed =
+      nextProps.statistic.recentGamesStatus === LoadStatus.FAILED &&
+      nextProps.statistic.recentGamesStatus !==
+        this.props.statistic.recentGamesStatus;
+    const isMostPopularGamesFailed =
+      nextProps.statistic.mostPopularGamesStatus === LoadStatus.FAILED &&
+      nextProps.statistic.mostPopularGamesStatus !==
+        this.props.statistic.mostPopularGamesStatus;
 
-    const isBestUsersInitFailed = nextProps.statistic.bestUsersStatus
-    === LoadStatus.FAILED && nextProps.statistic.bestUsersStatus !== this.props.statistic.bestUsersStatus;
-    const isRecentGamesInitFailed = nextProps.statistic.recentGamesStatus
-    === LoadStatus.FAILED && nextProps.statistic.recentGamesStatus !== this.props.statistic.recentGamesStatus;
-    const isMostPopularGamesFailed = nextProps.statistic.mostPopularGamesStatus
-    === LoadStatus.FAILED && nextProps.statistic.mostPopularGamesStatus !== this.props.statistic.mostPopularGamesStatus;
-
-    if(isBestUsersInitFailed) {
+    if (isBestUsersInitFailed) {
       this.dataForSnack.push({
         type: 'error',
         msg: 'User Init failed'
       });
     }
 
-    if(isRecentGamesInitFailed) {
+    if (isRecentGamesInitFailed) {
       this.dataForSnack.push({
         type: 'error',
         msg: 'Recent Games Init failed'
       });
     }
 
-    if(isMostPopularGamesFailed) {
+    if (isMostPopularGamesFailed) {
       this.dataForSnack.push({
         type: 'error',
         msg: 'Most Popular Games Init failed'
       });
     }
 
-    if( isBestUsersInitFailed || isRecentGamesInitFailed || isMostPopularGamesFailed ) {
+    if (
+      isBestUsersInitFailed ||
+      isRecentGamesInitFailed ||
+      isMostPopularGamesFailed
+    ) {
       this.props.openSnackbar();
     }
-
   }
-    
-  public closeSnackbar(): void{
+
+  public closeSnackbar(): void {
     this.props.closeSnackbar();
   }
 
@@ -74,7 +84,6 @@ class CaStatisticPageComponent extends React.Component<StatisticProps> {
   }
 
   public componentDidMount(): void {
-  
     if (this.props.authStatus === AuthStatus.NOT_AUTHORIZED) {
       this.props.history.push('/login');
     }
@@ -85,65 +94,62 @@ class CaStatisticPageComponent extends React.Component<StatisticProps> {
     this.props.history.push('/');
   }
 
-
-
   public render(): JSX.Element {
-const errorMessages = this.dataForSnack.filter(d => d.type = "error");
+    const errorMessages = this.dataForSnack.filter(d => (d.type = 'error'));
 
-    const isDataLoaded = (
-      this.props.statistic.bestUsersStatus === LoadStatus.COMPLETED && 
-      this.props.statistic.recentGamesStatus  === LoadStatus.COMPLETED && 
-      this.props.statistic.mostPopularGamesStatus  === LoadStatus.COMPLETED
-    );
+    const isDataLoaded =
+      this.props.statistic.bestUsersStatus === LoadStatus.COMPLETED &&
+      this.props.statistic.recentGamesStatus === LoadStatus.COMPLETED &&
+      this.props.statistic.mostPopularGamesStatus === LoadStatus.COMPLETED;
 
-    const isDataFailed = (
-      this.props.statistic.bestUsersStatus === LoadStatus.FAILED && 
-      this.props.statistic.recentGamesStatus  === LoadStatus.FAILED && 
-      this.props.statistic.mostPopularGamesStatus  === LoadStatus.FAILED
-    );    return (
+    const isDataFailed =
+      this.props.statistic.bestUsersStatus === LoadStatus.FAILED &&
+      this.props.statistic.recentGamesStatus === LoadStatus.FAILED &&
+      this.props.statistic.mostPopularGamesStatus === LoadStatus.FAILED;
+    return (
       <div className="ca-statistic">
         {this.props.children}
 
-<CaSnackbar
-          style={{top: '75px'}}
+        <CaSnackbar
+          style={{ top: '75px' }}
           anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-          open={ this.props.isSnackbarOpen }
-          autoHideDuration = {4000}
-          handleClose= {() => this.closeSnackbar()}
+          open={this.props.isSnackbarOpen}
+          autoHideDuration={4000}
+          handleClose={() => this.closeSnackbar()}
           type="error"
           message={
-          <React.Fragment>
-          {errorMessages.map((err: FrontEndSnackbarData, index: number) => 
-            <div key={index}>* {err.msg}</div>
-          )}
-          </React.Fragment>
-        }
+            <React.Fragment>
+              {errorMessages.map((err: FrontEndSnackbarData, index: number) => (
+                <div key={index}>* {err.msg}</div>
+              ))}
+            </React.Fragment>
+          }
           transitionDirection="down"
         />
 
         <CaSnackbar
-          style={{bottom: '50px'}}
+          style={{ bottom: '50px' }}
           anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-          open={ this.props.isSnackbarOpen }
-          autoHideDuration = {4000}
-          handleClose= {() => this.closeSnackbar()}
+          open={this.props.isSnackbarOpen}
+          autoHideDuration={4000}
+          handleClose={() => this.closeSnackbar()}
           type="info"
           message={
-          <React.Fragment>
-          {errorMessages.map((err: FrontEndSnackbarData, index: number) => 
-            <div key={index}>* {err.msg}</div>
-          )}
-          </React.Fragment>
-        }
+            <React.Fragment>
+              {errorMessages.map((err: FrontEndSnackbarData, index: number) => (
+                <div key={index}>* {err.msg}</div>
+              ))}
+            </React.Fragment>
+          }
           transitionDirection="up"
         />
-      {!isDataLoaded && !isDataFailed ?          
-      <div className="ca-homepage__spinner-container">
- 			  <CaSpinner isActive={!isDataLoaded} />  
-      </div>
-       :
-        <CaUsersTables statistic={this.props.statistic}/>
-      }
+        {!isDataLoaded && !isDataFailed ? (
+          <div className="ca-homepage__spinner-container">
+            <CaSpinner isActive={!isDataLoaded} />
+          </div>
+        ) : (
+          <CaUsersTables statistic={this.props.statistic} />
+        )}
       </div>
     );
   }
@@ -165,9 +171,7 @@ const mapDispatchToProps = (dispatch: any) => ({
   openSnackbar: () => dispatch(new OpenSnackbar())
 });
 
-
 export const CaStatisticPage = connect(
   mapStateToProps,
   mapDispatchToProps
-
 )(CaStatisticPageComponent);
