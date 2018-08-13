87bc98c22483b67dc70ec27fb7a97d33e5daf8e2
MINSK\Uladzimir_Pantsiukhou <uladzimir.pantsiukhou@gmail.com>
2018-08-13 15:44:45 +0300

    + fix merge conflicts

--- server/errors/errCodes.ts
+++ server/errors/errCodes.ts
@@ -1,18 +1,7 @@
 export enum errCodes {
-  smtRequired = 100,
-  smtInvalid = 101,
-  smtIsOutOfRange = 102,
-  smtIsNotEqual = 103,
-  smtExist = 104,
-  smtIsEqual = 105,
 }
 
 export enum technicalErrCodes {
-  saltIsNotGenerated = 500,
-  canNotBcryptString = 501,
-  smtIsNotFoundedInDb = 503,
-  smtIsNotUpsertedInDb = 504,
-}
 
 export interface Error {
   code: number;
