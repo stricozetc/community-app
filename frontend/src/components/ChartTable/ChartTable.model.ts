d7fe0378f8a5abd702558626b4748caa037773ca
Yegor Feoktistov <yegor_feoktistov1@epam.com>
2018-08-13 13:13:44 +0300

    fix: add switch case statement and service call in chart container

--- frontend/src/components/ChartTable/ChartTable.model.ts
+++ frontend/src/components/ChartTable/ChartTable.model.ts
@@ -1,5 +1,14 @@
 import { StatisticState } from 'store/statistic/interfaces';
 
+import { chartCategories } from './../../models';
+
 export interface ChartTableProps {
   statistics: StatisticState;
+  itemName: string;
+  chartList: string[];
+  chartCategory: chartCategories;
+}
+
+export interface ChartTableState {
+  chartName: string;
 }
--- frontend/src/components/ChartTable/ChartTable.tsx
+++ frontend/src/components/ChartTable/ChartTable.tsx
@@ -1,33 +1,103 @@
 import { WinRateDiagram } from 'components/WinRateDiagram';
 import * as React from 'react';
-import { I18n } from 'react-i18next';
+import { I18n, TranslationFunction } from 'react-i18next';
+import { ChartsService } from 'services/charts.service';
+
+import { StatisticState } from 'store';
+
+import { /* chartCategories, */ chartsTypes } from 'models';
 
 import { CaSelect } from '../form-controls/CaSelect';
 
-import { ChartTableProps } from './ChartTable.model';
+import { ChartTableProps, ChartTableState } from './ChartTable.model';
 import './ChartTable.scss';
 
-export class ChartTable extends React.Component<ChartTableProps> {
+export class ChartTable extends React.Component<ChartTableProps, ChartTableState> {
+  public constructor(props: ChartTableProps) {
+    super(props);
+
+    this.state = {
+      chartName: ''
+    };
+  }
+
+  public componentDidUpdate(prevProps: ChartTableProps): void {
+    if (prevProps.chartList !== this.props.chartList) {
+      this.setState({
+        chartName: this.props.chartList[0] || ''
+      });
+    }
+  }
+
+  public handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const chartName = event.target.value;
+
+    this.setState({
+      chartName
+    });
+  }
+
+  public setChart = (
+    chartName: string,
+    statistics: StatisticState,
+    itemName: string,
+    /* chartCategory: chartCategories, */
+    t: TranslationFunction
+  ) => {
+    let chartComponent: JSX.Element;
+
+    switch (chartName) {
+      case chartsTypes.winRate: {
+        const diagramData = ChartsService.getWinRateData(itemName, statistics.recentGames);
+
+        chartComponent = <WinRateDiagram diagramData={diagramData} />;
+
+        break;
+      }
+      default: {
+        chartComponent = (
+          <div className='chart-table__empty-block'>
+            {t('emptyChartMessage')}
+          </div>
+        );
+
+        break;
+      }
+    }
+
+    return chartComponent;
+  }
+
   public render(): JSX.Element {
-    const { statistics } = this.props;
+    // wrap chartCategory with comments for now, it can be used later
+    const { statistics, itemName, /* chartCategory, */ chartList } = this.props;
+
+    const isItemSelected = itemName.length !== 0 ? true : false;
 
     return (
       <I18n>
         {
           ( t ) => (
             <div className='chart-table'>
+              <div className='chart-table__title'>
+                {itemName}
+              </div>
               <div className='chart-table__head'>
                 <div className='chart-table__chart-select'>
-                  <CaSelect
-                    values={['winRate']}
-                    displayedValues={[t('winRate')]}
-                    handleChange={() => console.log('qdqw')}
-                    currentValue={'winRate'}
-                  />
+                  {
+                    isItemSelected
+                    ? <CaSelect
+                        values={chartList}
+                        displayedValues={chartList.map(item => t(item))}
+                        handleChange={this.handleSelectChange}
+                        currentValue={this.state.chartName}
+                      />
+                    : null
+                  }
                 </div>
               </div>
               <div className='chart-table__content'>
-                <WinRateDiagram gameData={statistics.recentGames} />
+                {this.setChart(this.state.chartName, statistics, itemName, /* chartCategory, */ t)}
               </div>
             </div>
           )
