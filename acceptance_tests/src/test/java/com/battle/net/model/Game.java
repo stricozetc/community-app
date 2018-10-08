000df48d15304a1389ea4ab4c84e9af48ba897c5
Yauheniya_Smoliak <Yauheniya_Smoliak@epam.com>
2018-10-08 15:23:48 +0300

    add editGame.feature

--- acceptance_tests/src/test/java/com/battle/net/model/Game.java
+++ acceptance_tests/src/test/java/com/battle/net/model/Game.java
@@ -5,25 +5,28 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Comparator;
+import java.util.Objects;
+
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
-public class Game {
+public class Game implements Comparable {
     private Integer id;
     private Integer userId;
     private String appName;
     private String description = "Test game for testing";
-    private int maxRoomPlayer = 2;
-    private int maxRooms = 1;
+    private Integer maxRoomPlayer = 2;
+    private Integer maxRooms = 1;
     private String requestUrl = "http://localhost:8040";
-    private int maxWaitingTime = 20;
+    private Integer maxWaitingTime = 20;
     private String redirectUrl = "http://localhost:8040/home";
     private String registrationEventName = "onFlappyBirdTest";
     private String leaveEventName = "onLeaveFlappyBirdTest";
     private String updateRoomsInfoEventName = "onUpdateRoomsInfoFlappyBirdTest";
     private String notifyCountdown = "onNotifyCountdownFlappyBirdTest";
-    private boolean approve = true;
+    private Boolean approve = true;
     private String appToken;
     private String updatedAt;
     private String createdAt;
@@ -31,5 +34,35 @@ public class Game {
     public Game(int userId, String appName) {
         this.userId = userId;
         this.appName = appName;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return Comparator.comparing(Game::getId)
+                .thenComparing(Game::getUserId)
+                .thenComparing(Game::getAppName)
+                .thenComparing(Game::getDescription)
+                .thenComparing(Game::getMaxRoomPlayer)
+                .thenComparing(Game::getMaxRooms)
+                .thenComparing(Game::getRequestUrl)
+                .thenComparing(Game::getMaxWaitingTime)
+                .thenComparing(Game::getRedirectUrl)
+                .thenComparing(Game::getRegistrationEventName)
+                .thenComparing(Game::getLeaveEventName)
+                .thenComparing(Game::getUpdateRoomsInfoEventName)
+                .thenComparing(Game::getNotifyCountdown)
+                .thenComparing(Game::getApprove)
+                .compare(this, (Game) o);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return compareTo(o) == 0 ? true : false;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, userId, appName, description, maxRoomPlayer, maxRooms, requestUrl, maxWaitingTime, redirectUrl, registrationEventName, leaveEventName, updateRoomsInfoEventName, notifyCountdown, approve, appToken, updatedAt, createdAt);
     }
 }
--- acceptance_tests/src/test/java/com/battle/net/steps/GameSteps.java
+++ acceptance_tests/src/test/java/com/battle/net/steps/GameSteps.java
@@ -7,6 +7,8 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
 
+import java.util.Map;
+
 public class GameSteps {
     private Container container;
 
@@ -26,4 +28,17 @@ public class GameSteps {
         Assert.assertEquals(container.response.statusCode(), 200);
     }
 
+    @When("^User edit the game with new parameters$")
+    public void userEditTheGameWithNewParameters(Map<String, String> newParams) {
+        container.game.setDescription(newParams.get("description"));
+        container.game.setMaxRoomPlayer(Integer.valueOf(newParams.get("maxRoomPlayer")));
+        container.response = GameService.editGame(container.game);
+    }
+
+    @Then("^The game is edited successfully$")
+    public void theGameIsEditedSuccessfully() {
+        container.response = GameService.getGame(container.user);
+        Game editedGame = container.response.jsonPath().getObject("find { it.appName.equals('" + container.game.getAppName() + "')}", Game.class);
+        Assert.assertEquals(container.game, editedGame);
+    }
 }
