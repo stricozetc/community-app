66c41afde8b90b7221f062dfdd7fab5a12dfa01b
AliaksandrMirankou <stricozetc@mail.ru>
2019-03-18 19:34:18 +0300

    deleted unused properties

--- frontend/src/models.ts
+++ frontend/src/models.ts
@@ -338,14 +338,12 @@ export interface Margin {
 
 export interface Event {
   id?: number;
-  //userId?: number;
   title: string;
   description: string;
   city: string;
   place: string;
   address: string;
   location: string;
-  //locationY: string;
   begginingInTime: string;
   begginingDate: string;
   createAt?: Date;
