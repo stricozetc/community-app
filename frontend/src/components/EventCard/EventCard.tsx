deea0f3d49198524a030e98078049c4d6a3fd53f
AliaksandrMirankou <stricozetc@mail.ru>
2019-03-18 20:53:56 +0300

    used localisation

--- frontend/src/components/EventCard/EventCard.tsx
+++ frontend/src/components/EventCard/EventCard.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { I18n } from 'react-i18next';
 
 import './EventCard.scss';
 import { EventCardProps } from './EventCard.model';
@@ -12,9 +13,15 @@ export class CaEventCard extends React.Component<EventCardProps> {
         <button className='ca-about_card_edit-btn'>Edit</button>
         <button className='ca-about_card_delete-btn'>+</button>
         <div className='ca-about_card_img'>
-          <NavLink to={`/event/${id}`} >
-            <button className='ca-about_card_img_btn'>JOIN US</button>
-          </NavLink>
+          <I18n>
+            {
+              t => (
+                <NavLink to={`/event/${id}`} >
+                  <button className='ca-about_card_img_btn'>{t('JOIN US')}</button>
+                </NavLink>
+              )
+            }
+          </I18n>
         </div>
         <div className='ca-about_card_information'>
           <div className='ca-about_card_information_date'>
