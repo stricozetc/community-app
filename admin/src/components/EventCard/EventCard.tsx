cf4dd43d1452f6ae53364f84c0f146182a4edb24
AliaksandrMirankou <stricozetc@mail.ru>
2019-03-26 01:03:21 +0300

    corrected event card

--- admin/src/components/EventCard/EventCard.tsx
+++ admin/src/components/EventCard/EventCard.tsx
@@ -1,25 +1,17 @@
 import * as React from 'react';
-import { I18n } from 'react-i18next';
 
 import './EventCard.scss';
 import { EventCardProps } from './EventCard.model';
-import { NavLink } from 'react-router-dom';
+
 
 export class CaEventCard extends React.PureComponent<EventCardProps> {
   render() {
-    const { id, title, city, place, begginingInTime, begginingDate } = this.props;
+    const { title, city, place, begginingInTime, begginingDate } = this.props;
     return (
       <div className='ca-about_card'>
+        <button className='ca-about_card_edit-btn'>Edit</button>
+        <button className='ca-about_card_delete-btn'>+</button>
         <div className='ca-about_card_img'>
-          <I18n>
-            {
-              t => (
-                <NavLink to={`/event/${id}`} >
-                  <button className='ca-about_card_img_btn'>{t('JOIN US')}</button>
-                </NavLink>
-              )
-            }
-          </I18n>
         </div>
         <div className='ca-about_card_information'>
           <div className='ca-about_card_information_date'>
