deeeb1c47fb9b0b1457db08c162e32b2b5b0481a
AliaksandrMirankou <stricozetc@mail.ru>
2019-03-26 15:41:53 +0300

    added control buttons for event card in admin console

--- admin/src/components/EventCard/EventCard.tsx
+++ admin/src/components/EventCard/EventCard.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react';
+import { I18n } from 'react-i18next';
 
 import './EventCard.scss';
 import { EventCardProps } from './EventCard.model';
-
+import { NavLink } from 'react-router-dom';
 
 export class CaEventCard extends React.PureComponent<EventCardProps> {
   render() {
-    const { title, city, place, begginingInTime, begginingDate } = this.props;
+    const { id, title, city, place, begginingInTime, begginingDate } = this.props;
     return (
       <div className='ca-about_card'>
-        <button className='ca-about_card_edit-btn'>Edit</button>
+        <I18n>
+          {
+            t => (
+              <NavLink to={`/events/edit-event/${id}`}>
+                <button className='ca-about_card_edit-btn'>{t('Edit')}</button>
+              </NavLink>
+            )
+          }
+        </I18n>
         <button className='ca-about_card_delete-btn'>+</button>
         <div className='ca-about_card_img'>
         </div>
