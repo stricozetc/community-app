6c1d75fc2b4f2b169f8fba4456878da049a68f49
AliaksandrMirankou <stricozetc@mail.ru>
2019-04-04 13:07:28 +0300

    fixed BEM indents

--- admin/src/components/EventCard/EventCard.tsx
+++ admin/src/components/EventCard/EventCard.tsx
@@ -9,37 +9,37 @@ export class CaEventCard extends React.PureComponent<EventCardProps> {
   render() {
     const { id, title, city, place, begginingInTime, begginingDate } = this.props;
     return (
-      <div className='ca-about_card'>
+      <div className='ca-about-card'>
         <I18n>
           {
             t => (
               <NavLink to={`/events/edit-event/${id}`}>
-                <button className='ca-about_card_edit-btn'>{t('Edit')}</button>
+                <button className='ca-about-card__edit-btn'>{t('Edit')}</button>
               </NavLink>
             )
           }
         </I18n>
-        <button className='ca-about_card_delete-btn'>+</button>
-        <div className='ca-about_card_img'>
+        <button className='ca-about-card__delete-btn'>+</button>
+        <div className='ca-about-card__img'>
         </div>
-        <div className='ca-about_card_information'>
-          <div className='ca-about_card_information_date'>
-            <div className='ca-about_card_information_date_month'>
+        <div className='ca-about-card__information'>
+          <div className='ca-about-card__information-date'>
+            <div className='ca-about-card__information-date-month'>
               {begginingDate}
             </div>
-            <div className='ca-about_card_information_date_num'>
+            <div className='ca-about-card__information-date-num'>
               {begginingInTime}
             </div>
           </div>
-          <div className='ca-about_card_information_text'>
-            <div className='ca-about_card_information_text_title'>
+          <div className='ca-about-card__information-text'>
+            <div className='ca-about-card__information-text-title'>
               {title}
             </div>
-            <div className='ca-about_card_information_text_time-address'>
-              <div className='ca-about_card_information_text_time'>
+            <div className='ca-about-card__information-text-time-address'>
+              <div className='ca-about-card__information-text-time'>
                 {city}
               </div>
-              <div className='ca-about_card_information_text_address'>
+              <div className='ca-about-card__information-text-address'>
                 {place}
               </div>
             </div>
